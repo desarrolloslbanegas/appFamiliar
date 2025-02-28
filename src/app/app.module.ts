c68b429db34f34aa42d124a67e29b347b4f91221
Banegas Lucas Matias <lmbanegas@anses.gob.ar>
2025-02-28 16:04:17 -0300

    Inicio de aplicación

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [AppComponent, NavbarComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
